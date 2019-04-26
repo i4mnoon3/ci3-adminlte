34006eb02ab197dd7e0a50964bef5ef8412dadbc
Ian Escarro <ian.escarro@gmail.com>
2019-04-26 08:33:32 +0800

    Initial commit

---
+++ application/libraries/Layout.php
@@ -0,0 +1,30 @@
+<?php
+
+class Layout {
+
+  function __construct($layout = 'layout2', $theme = 'AdminLTE') {
+    $this->layout = $layout;
+    $this->theme = $theme;
+    $this->obj = &get_instance();
+    $this->data = array();
+  }
+
+  function set_data($key, $value) {
+    $this->data[$key] = $value;
+  }
+
+  function view($view, $data = array()) {
+    $data = array_merge($this->data, $data);
+    $data['content'] = $this->obj->load->view($this->theme . '/' . $view, $data, TRUE);
+    $this->obj->load->view($this->theme . '/' . $this->layout, $data);
+  }
+
+  function set_layout($layout) {
+    $this->layout = $layout;
+  }
+
+  function get_theme() {
+    return $this->theme;
+  }
+
+}
